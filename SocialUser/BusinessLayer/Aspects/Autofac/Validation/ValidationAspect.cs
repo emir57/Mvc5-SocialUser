f24bbe329ec6c015c405180255cc5833b42bab63
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:36:19 +0300

    Complete ValidationAspect

--- SocialUser/BusinessLayer/Aspects/Autofac/Validation/ValidationAspect.cs
+++ SocialUser/BusinessLayer/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -1,4 +1,7 @@
 ﻿using BusinessLayer.Utilities.Interceptor;
+using BusinessLayer.Utilities.ValidationTool;
+using Castle.DynamicProxy;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,25 @@ namespace BusinessLayer.Aspects.Autofac.Validation
 {
     public class ValidationAspect:MethodInterception
     {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new Exception("Validasyon için geçersiz tip");
+            }
+            _validatorType = validatorType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var context = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(x => x.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(context, entity);
+            }
+        }
     }
 }
