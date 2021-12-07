c61df8ddd13f152b74af52e6e5822b1b075a9f59
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:08:46 +0300

    Complete ValidationTool

--- SocialUser/BusinessLayer/Utilities/ValidationTool/ValidationTool.cs
+++ SocialUser/BusinessLayer/Utilities/ValidationTool/ValidationTool.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace BusinessLayer.Utilities.ValidationTool
 {
     public class ValidationTool
     {
+        public static void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
     }
 }
