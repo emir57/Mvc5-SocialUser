8a84bc6ada19450b36955ae842c911bed352328c
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:33:00 +0300

    Add ValidationAspect

---
+++ SocialUser/BusinessLayer/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Utilities.Interceptor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Aspects.Autofac.Validation
+{
+    public class ValidationAspect:MethodInterception
+    {
+    }
+}
