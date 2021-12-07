62699430726707514bddad641e9ee381bc1f7230
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:23:15 +0300

    MethodInterception

---
+++ SocialUser/BusinessLayer/Utilities/Interceptor/MethodInterception.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Utilities.Interceptor
+{
+    public class MethodInterception:MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation,Exception e) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnBefore(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
