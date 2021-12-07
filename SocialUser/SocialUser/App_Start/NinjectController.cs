71f1fdf6e55a3533bacd9fc89964e56eeefaabbf
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 21:15:56 +0300

    Add Ninject Dependency Injection

---
+++ SocialUser/SocialUser/App_Start/NinjectController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.DependencyResolvers.Ninject;
+using BusinessLayer.Utilities;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SocialUser.App_Start
+{
+    public class NinjectController : DefaultControllerFactory
+    {
+        private IKernel _kernel;
+        public NinjectController()
+        {
+            _kernel = new StandardKernel(new BusinessModule());
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)_kernel.Get(controllerType);
+        }
+    }
+}
