4648a43cd61e9af7c2dfc2f222508af924557e2d
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:20:44 +0300

    Add MethodInterceptionBaseAttribute

--- SocialUser/SocialUser/Global.asax.cs
+++ SocialUser/SocialUser/Global.asax.cs
@@ -18,6 +18,7 @@ namespace SocialUser
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ControllerBuilder.Current.SetControllerFactory(new NinjectController());
+            //ControllerBuilder.Current.SetControllerFactory();
         }
     }
 }
