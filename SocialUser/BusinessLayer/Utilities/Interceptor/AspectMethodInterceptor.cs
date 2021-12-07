310f9f0df7e01a0cd63534fe54b67c69ececc127
emir57 <emir.gurbuz06@hotmail.com>
2021-12-07 22:25:25 +0300

    AspectMethodInterceptor

---
+++ SocialUser/BusinessLayer/Utilities/Interceptor/AspectMethodInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Utilities.Interceptor
+{
+    public class AspectMethodInterceptor : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttibutes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttibutes.AddRange(methodAttributes);
+
+            return classAttibutes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
